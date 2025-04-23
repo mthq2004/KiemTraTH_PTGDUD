9d66b77f48d284756f151583dfcb770e01bba2a3
H51M14\Student <maithanhhaiquan2004@gmail.com>
2025-04-23 15:52:11 +0700

    hien thi danh sach mau

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([
+    { id: 1, title: "Lão Hạc", author: "Nam Cao", genre: "Văn học", year: 1943 },
+    { id: 2, title: "Sapiens", author: "Yuval Noah Harari", genre: "Khoa học", year: 2011 }
+  ]);
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>Danh sách sách</h1>
+      {books.map(book => (
+        <div key={book.id}>
+          <p>{book.title} - {book.author} - {book.genre} - {book.year}</p>
+          <button>Xoá</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
